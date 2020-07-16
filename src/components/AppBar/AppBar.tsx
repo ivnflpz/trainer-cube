a1e7930973719babd710c04b09c3bdfac349967d
Ivan Lopez <ivan.lopez@wpengine.com>
2020-07-16 02:01:01 -0500

    refactor: move user actions to own component

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -3,15 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import MuiAppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { signInWithGoogle, signOut } from '../../firebase';
-import { useUserContext } from '../../providers/UserProvider';
-import Avatar from '@material-ui/core/Avatar';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
+import UserActions from '../UserActions';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,62 +29,6 @@ interface IAppBarProps {
 
 const AppBar: React.FC<IAppBarProps> = ({ onMenuToggle }) => {
   const classes = useStyles();
-  const {user} = useUserContext();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-
-  const handleMenu = (event: any) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const renderActions = (): React.ReactElement => {
-    if (user !== null && user !== undefined) {
-      return renderUserActions();
-    } else {
-      return <Button color="inherit" onClick={signInWithGoogle}>Login</Button>;
-    }
-  };
-
-  const renderUserActions = () => {
-    const { displayName, photoURL } = user;
-    const userIcon = photoURL !== null ? <Avatar className={classes.avatar} alt={displayName} src={photoURL} /> : <AccountCircle />;
-    return (
-      <div>
-        <IconButton
-          aria-label="account of current user"
-          aria-controls="menu-appbar"
-          aria-haspopup="true"
-          onClick={handleMenu}
-          color="inherit"
-        >
-          {userIcon}
-        </IconButton>
-        <Menu
-          id="menu-appbar"
-          anchorEl={anchorEl}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          open={open}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose}>Profile</MenuItem>
-          <MenuItem onClick={handleClose}>My account</MenuItem>
-          <MenuItem onClick={signOut}>Logout</MenuItem>
-        </Menu>
-      </div>
-    );
-  };
 
   return (
     <div className={classes.root}>
@@ -102,7 +40,7 @@ const AppBar: React.FC<IAppBarProps> = ({ onMenuToggle }) => {
           <Typography variant="h6" className={classes.title}>
             Trainer<sup>3</sup>
           </Typography>
-          {renderActions()}
+          <UserActions />
         </Toolbar>
       </MuiAppBar>
     </div>
