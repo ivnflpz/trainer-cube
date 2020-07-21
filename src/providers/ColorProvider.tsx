478a56eca981a7d6f4bd06e89e130dc18198f53c
Ivan Lopez <ivan.lopez@wpengine.com>
2020-07-21 17:43:12 -0500

    feat: customizable cube scheme with color picker in settings

--- src/providers/ColorProvider.tsx
+++ src/providers/ColorProvider.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 interface IColorContext {
     colors: any;
+    updateColors: (face: string, value: string) => void;
 }
 
 const ColorContext = React.createContext<IColorContext>({ 
-  colors: null
+  colors: null,
+  updateColors: () => null,
 });
 
+const COLORS_KEY = 'trainerColors';
 const ColorProvider: React.FC = ({children}) => {
   const [sColors, setColors] = React.useState({U: 'yellow',
     D: 'white',
@@ -16,8 +19,24 @@ const ColorProvider: React.FC = ({children}) => {
     R: 'green',
     L: 'blue',
     N: 'gray' });
+
+  React.useEffect(() => {
+    const lsColorsString = localStorage.getItem(COLORS_KEY);
+    if (lsColorsString !== null) {
+      setColors(JSON.parse(lsColorsString));
+    }
+  }, []);
+
+  React.useEffect(() => {
+    localStorage.setItem(COLORS_KEY, JSON.stringify(sColors));
+  }, [sColors]);
+
+  const updateColors = (face: string, value: string) => {
+    setColors(colors => { return {...colors, [face]: value}; });
+  };
+    
   return (
-    <ColorContext.Provider value={{ colors: sColors }}>
+    <ColorContext.Provider value={{ colors: sColors, updateColors }}>
       {children}
     </ColorContext.Provider>
   );
