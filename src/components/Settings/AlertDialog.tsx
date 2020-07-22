0c7894667fc55e14f36b01f5bcde1ebb2a50225b
Ivan Lopez <ivan.lopez@wpengine.com>
2020-07-21 19:36:26 -0500

    feat: add button to reset color scheme

---
+++ src/components/Settings/AlertDialog.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+interface IAlertDialogProps {
+  children: React.ReactElement;
+  open: boolean;
+  onClose: () => void;
+  onAccept: () => void;
+  title: string;
+}
+const AlertDialog: React.FC<IAlertDialogProps> = ({ children, open, onAccept, onClose, title }) => {
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        {children}
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="primary">
+            Disagree
+        </Button>
+        <Button onClick={onAccept} color="primary" autoFocus>
+            Agree
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default AlertDialog;
--- src/providers/ColorProvider.tsx
+++ src/providers/ColorProvider.tsx
@@ -3,22 +3,27 @@ import React from 'react';
 interface IColorContext {
     colors: any;
     updateColors: (face: string, value: string) => void;
+    resetToDefault: () => void;
 }
 
 const ColorContext = React.createContext<IColorContext>({ 
   colors: null,
   updateColors: () => null,
+  resetToDefault: () => null,
 });
 
 const COLORS_KEY = 'trainerColors';
+const defaultColors = {U: 'yellow',
+  D: 'white',
+  F: 'red',
+  B: 'orange',
+  R: 'green',
+  L: 'blue',
+  N: 'gray' 
+};
+
 const ColorProvider: React.FC = ({children}) => {
-  const [sColors, setColors] = React.useState({U: 'yellow',
-    D: 'white',
-    F: 'red',
-    B: 'orange',
-    R: 'green',
-    L: 'blue',
-    N: 'gray' });
+  const [sColors, setColors] = React.useState(defaultColors);
 
   React.useEffect(() => {
     const lsColorsString = localStorage.getItem(COLORS_KEY);
@@ -34,9 +39,13 @@ const ColorProvider: React.FC = ({children}) => {
   const updateColors = (face: string, value: string) => {
     setColors(colors => { return {...colors, [face]: value}; });
   };
+
+  const resetToDefault = () => {
+    setColors(defaultColors);
+  };
     
   return (
-    <ColorContext.Provider value={{ colors: sColors, updateColors }}>
+    <ColorContext.Provider value={{ colors: sColors, updateColors, resetToDefault }}>
       {children}
     </ColorContext.Provider>
   );
